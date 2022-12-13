c05ad81ef16d45e9d7ad5130957687e17969e221
Julio Cezar <julio.cezar@liferay.com>
2022-12-13 12:36:25 -0300

    Fix Bug

--- app.js
+++ app.js
@@ -22,25 +22,24 @@ function loadEventListeners() {
 function addTask(e) {
   if (taskInput.value === "") {
     alert("Add a task");
+  } else {
+    e.preventDefault();
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    //store in LS
+    storeTaskInLocalStorage(taskInput.value);
+
+    taskInput.value = "";
   }
-
-  const li = document.createElement("li");
-  li.className = "collection-item";
-  li.appendChild(document.createTextNode(taskInput.value));
-
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-
-  li.appendChild(link);
-  taskList.appendChild(li);
-
-  //store in LS
-  storeTaskInLocalStorage(taskInput.value);
-
-  taskInput.value = "";
-
-  e.preventDefault();
 }
 
 //store Task
